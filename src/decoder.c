4efcd24f3aa28ff9ce481a95523a7e34483358ce
Steffen Schulz <steffen.schulz@intel.com>
2021-08-06 06:28:49 -0700

    do not attempt to disassemble blocks just because of trace_mode=1
    Trace decode may fails on some cases where PT stream has PGE+PGD for
    some address but the address is never fetched/disassembled in real execution.
    
    Attempting to trace such page_cache + pt_dump inputs later on fails with
    page_fault while it seems to produce good traces with this patch.

--- src/decoder.c
+++ src/decoder.c
@@ -356,8 +356,8 @@ static inline uint64_t get_val(uint8_t **pp, uint8_t len){
 static inline void disasm(decoder_t* self){
 	static uint64_t failed_page = 0;
 	should_disasm_t* res = self->decoder_state_result;
-	if(res->valid && (!is_empty_tnt_cache(self->tnt_cache_state) || self->disassembler_state->trace_mode)){
-    	LOGGER("\n\ndisasm(%lx,%lx)\tTNT: %ld\n", res->start, res->end, count_tnt(self->tnt_cache_state));
+	if(res->valid && (!is_empty_tnt_cache(self->tnt_cache_state))){
+		LOGGER("disasm(%lx,%lx)\tTNT: %d\n", res->start, res->end, count_tnt(self->tnt_cache_state));
 			if(unlikely(trace_disassembler(self->disassembler_state, res->start, res->end, self->tnt_cache_state, &failed_page, self->mode) == disas_page_fault)){
 				self->page_fault_found = true;
 				self->page_fault_addr = failed_page;
